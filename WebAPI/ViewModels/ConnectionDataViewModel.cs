73e9327a49a84a9d30fda0d8a1996bf57986292c
fastjonh <fastjonh@gmail.com>
2023-09-01 19:22:25 -0300

    adding yawapi project

--- WebAPI/ViewModels/ConnectionDataViewModel.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace WebAPI.ViewModels
-{
-    public record ConnectionDataViewModel(string Name, string PublicKey);
-
-    public record AuctionStartViewModel(string ProdutctDescription, double StartingValue, double MinBid, double MinutesDueTime);
-}
